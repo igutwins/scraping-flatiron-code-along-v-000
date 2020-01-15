508bd60afd7283b826dbda3d0ce9426ff2b91e70
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-15 04:33:47 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,19 +1,3 @@
 class Course
-@@all = []
-attr_accessor :title, :schedule, :description
 
-def initialize
-  @@all << self
-end
-
-def self.all
-  @@all
-end
-
-def self.reset_all
-  @@all.clear
-end
-
-
-
-end
+end 
