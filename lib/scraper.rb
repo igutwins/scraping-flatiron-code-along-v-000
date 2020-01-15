240f9addfce2d8e6a24a42278fab1a8f02792914
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-15 04:41:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,20 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
-end 
+
+def get_page
+end
+
+def get_courses
+end
+
+def make_courses
+end
+
+def print_courses
+end
+
+
+end
